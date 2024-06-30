5fa22fc4b65a36cffbcc4a04f085ba441380d253
chan <opokonse123@naver.com>
2024-06-30 21:31:54 +0900

    프로젝트 설계 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: false, //엄격 모드 false
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 eslint 오류 무시
+  },
 };
 export default nextConfig;
--- src/libs/Redux/ReduxProvier.client.tsx
+++ src/libs/Redux/ReduxProvier.client.tsx
@@ -9,6 +9,7 @@ type Props = {
 };
 const ReduxProvider = ({children}: Props) => {
   const storeRef = useRef<AppStore>();
+
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
